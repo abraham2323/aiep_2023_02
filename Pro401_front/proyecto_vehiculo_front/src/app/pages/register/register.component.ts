f58f2f8f386e6ffb4a71a4084e6aa230c5d62263
ABRAHAM NEFTALI BASAEZ FERRADA <ABRAHAM.BASAEZ@correoaiep.cl>
2023-10-01 00:41:13 -0300

    arreglo antes de morir

--- Pro401_front/proyecto_vehiculo_front/src/app/pages/register/register.component.ts
+++ Pro401_front/proyecto_vehiculo_front/src/app/pages/register/register.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/services/account.service';
+
+export function matchPasswordValidator(passwordKey:string, confirmPasswordKey:string) : ValidatorFn{
+  return (control : AbstractControl) : {[key : string] : any} | null => {
+    const password = control.get(passwordKey)?.value;
+    const confirmPassword = control.get(confirmPasswordKey)?.value;
+    return password === confirmPassword ? null : {passwordMismatch : true};
+  } 
+}
 
 @Component({
   selector: 'app-register',
@@ -16,6 +24,8 @@ export class RegisterComponent  implements OnInit {
     email : new FormControl('', [Validators.required, Validators.email]),
     password : new FormControl('',[Validators.required, Validators.pattern('^(?=.*[A-Z])[a-z\d]*(?=.*[^A-Za-z0-9]{2,}).{8,}$')]),
     confirmPassword : new FormControl('')
+  },{
+    validator: matchPasswordValidator('password', 'confirmPassword')
   })
 
   ngOnInit() {}
